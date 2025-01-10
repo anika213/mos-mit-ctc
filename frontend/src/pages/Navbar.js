a43b9b0acf5bd389bcce2b7ab3b9fefc36cbdd4e
Zachary Wood <person.named.zachary@gmail.com>
2025-01-10 15:32:23 -0800

    Relative -> absolute redirects

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -23,8 +23,8 @@ const Navbar = () => {
           <div className="text-2xl">☰</div>
         </div>
         <ul className={`md:flex md:items-center md:space-x-6 ${isMobileMenuOpen ? 'block' : 'hidden'} md:block`}>
-          <li><Link to="./home" className="hover:text-gray-400">Home</Link></li>
-          <li><Link to="./leaderboard" className="hover:text-gray-400">Leaderboard</Link></li>
+          <li><Link to="/home" className="hover:text-gray-400">Home</Link></li>
+          <li><Link to="/leaderboard" className="hover:text-gray-400">Leaderboard</Link></li>
           <li className="relative group">
             <span 
               className="cursor-pointer hover:text-gray-400" 
@@ -35,12 +35,12 @@ const Navbar = () => {
             </span>
             {isDropdownOpen && (
               <ul className="absolute left-0 mt-2 bg-black text-white shadow-lg rounded-md w-40">
-                <li className="p-2 hover:bg-gray-700"><Link to="./challenge/rna">RNA Challenge</Link></li>
-                <li className="p-2 hover:bg-gray-700"><Link to="./challenge/rna">Challenge 2</Link></li>
+                <li className="p-2 hover:bg-gray-700"><Link to="/challenge/rna">RNA Challenge</Link></li>
+                <li className="p-2 hover:bg-gray-700"><Link to="/challenge/rna">Challenge 2</Link></li>
               </ul>
             )}
           </li>
-          <li><Link to="./login" className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">Login</Link></li>
+          <li><Link to="/login" className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600">Login</Link></li>
         </ul>
       </div>
     </nav>
