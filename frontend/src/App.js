8b192d4b72270b341d9fb7f0ae83d93c3df2775f
Zachary Wood <person.named.zachary@gmail.com>
2025-01-11 00:44:23 -0800

    Registration page
    Basic registration page that adds users to our db.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
+<<<<<<< Updated upstream
 import Leaderboard from './pages/Leaderboard'
+=======
+import Register from './pages/Register';
+>>>>>>> Stashed changes
 import Challenge from './pages/Challenge';
 import RNAChallenge from './pages/challenges/RNAChallenge';
 
@@ -12,7 +16,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
+<<<<<<< Updated upstream
         <Route path="/leaderboard" element={<Leaderboard />}/>
+=======
+        <Route path="/register" element={<Register />} />
+>>>>>>> Stashed changes
         <Route path="/challenge" element={<Challenge />}>
           <Route path='rna' element={<RNAChallenge/>}/>
         </Route>
---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,87 @@
+import './Landing.module.css';
+import styles from './Register.module.css';
+import dna from '../assets/dna.jpg';
+import Navbar from './Navbar.js'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// TODO: whne the screen size is smaller, the side image shld be above the form, rn its below..
+// TODO: impelement all the functionality.
+function Register() {
+    // const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    // const [passwordConfirmation, setPasswordConfirmation] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await fetch('http://localhost:8080/users/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username,
+            password,
+        }),
+        credentials: 'include'
+        });
+
+        if (response.ok) {
+        navigate('/login');
+        } else {
+        const result = await response.json();
+        setError(result.message || 'Registration failed.');
+        }
+    } catch(err) {
+        console.log(`Error registering: ${err}`);
+        setError('Some error');
+    }
+    }
+
+    return (
+    <div className={styles.mainContainer}>
+        <Navbar />
+        <div className={styles.content}>
+        <div className={styles.container}>
+            <p className={styles.heading}>Register</p>
+            <p className={styles.subheading}>Please register to store your challenge progress and view your rank on our leaderboard.</p>
+            <form className={styles.form} onSubmit={handleRegistration}>
+            <input 
+            className={styles.input} 
+            type="text" 
+            id="username" 
+            name="username" 
+            placeholder='Username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} />
+
+            <input 
+            className={styles.input} 
+            type="password" 
+            id="password" 
+            name="password" 
+            placeholder='Password' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+
+            <button className={styles.button} type="submit">Register</button>
+            </form>
+
+            {error && <p className={styles.subheading}>{error}</p>}
+
+            <button className={styles.guestButton}>Continue as Guest</button>
+        </div>
+        <div className={styles.sideImageContainer}>
+            <img src={dna} alt="Side" className={styles.sideImage} />
+        </div>
+        </div>
+    </div>
+    );
+}
+
+export default Register;
