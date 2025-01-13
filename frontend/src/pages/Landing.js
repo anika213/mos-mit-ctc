1b174eeb5db0efbcf9a7172f70a3a520a4c90d15
ISADELLA\isave <ivera@g.hmc.edu>
2025-01-12 20:55:36 -0800

    view challenges on landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import styles from './Landing.module.css';
 import Navbar from './Navbar.js';
 import hmc from '../assets/hmc.png';
@@ -6,13 +7,29 @@ import mit from '../assets/mit.png';
 import mos from '../assets/mos.jpg';
 import dna from '../assets/dna.jpg';
 import { useNavigate } from 'react-router-dom';
+
+
+const challenges = [
+  { id: 1, title: "RNA Splicing", description: "Description for Challenge #1", link: "/challenge/rnasplicing" },
+  { id: 2, title: "Molecular Docking", description: "Description for Challenge #2", link: "/challenge/moleculardocking" },
+  { id: 3, title: "Wireless Detecting", description: "Description for Challenge #3", link: "/challenge/wirelessdetecting" },
+];
+
+
 // TODO: fix the button sizing, kinda weird when changing screen size rn
 // TODO: add the challenges at the bottom as drop down (FAQ style)
 function Landing() {
   const navigate = useNavigate();
+
   function goToChallenge() {
     navigate('/challenge');
   }
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const toggleChallenge = (id) => {
+    setActiveChallenge(activeChallenge === id ? null : id);
+  };
+
   return (
     <div className={styles.mainContainer}>
       <Navbar />
@@ -34,6 +51,31 @@ function Landing() {
         <div className={styles.sideImageContainer}>
           <img src={dna} alt="Side" className={styles.sideImage} />
         </div>
+      </div>
+      <div className={styles.challengescontainer}>
+        <h1 style={{ textAlign: "right" }}>Explore Challenges</h1>
+
+
+        {challenges.map((challenge) => (
+          <div key={challenge.id} style={{ marginBottom: "10px" }}>
+            <div
+              className={styles.closedchallenge}
+              onClick={() => toggleChallenge(challenge.id)}
+            >
+              <span><h2>{challenge.title}</h2></span>
+              <span>{activeChallenge === challenge.id ? "▲" : "▼"}</span>
+            </div>
+            {activeChallenge === challenge.id && (
+              <div className={styles.openchallenge}>
+                <p>{challenge.description}</p>
+                <br/>
+                <a href={challenge.link} className={styles.button} style={{ width: "10%"}}>
+                  Try it out!
+                </a>
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
