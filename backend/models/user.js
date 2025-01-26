ac9c3897043c13c9753f471bf13cef455d583c11
anika213 <sharm51155@gapps.uwcsea.edu.sg>
2025-01-26 00:28:48 -0800

    new challenge page

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const { Schema, model, Types } = mongoose;
 
-// Probs need to change this depending on how we want to store passwords etc, just a placeholder for now
 
 const schema = new Schema({
   username: {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const express = require ("express"); 
 
 const userRoutes = require ("./users");
+const challengeRoutes = require ("./challenges");
 
 const router = express.Router (); 
 
 router.use ("/users", userRoutes);
+router.use ("/challenges", challengeRoutes);
 
 module.exports = router; 
