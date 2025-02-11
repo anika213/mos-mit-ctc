5ad61a0886483becd36fc964052104c19b7d9ace
ISADELLA\isave <ivera@g.hmc.edu>
2025-02-11 12:01:09 -0800

    pop up to rotate phone

--- frontend/src/pages/Challenge.js
+++ frontend/src/pages/Challenge.js
@@ -71,6 +71,24 @@ function Challenge() {
   }, [challengeName, updateLevelAndStartTime]);
 
   const { title, description } = challengeData[challengeName][selectedLevel];
+  
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleOrientationChange = () => {
+    const isPortrait = window.matchMedia("(orientation: portrait)").matches;
+    setShowPopup(isPortrait);
+  };
+  window.addEventListener("resize", handleOrientationChange);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleOrientationChange);
+    // Initial check in case the page loads in landscape mode
+    handleOrientationChange();
+    return () => window.removeEventListener("resize", handleOrientationChange);
+  }, []);
+
+  
+  
 
   const onComplete = useCallback(() => {
     // mark challenge as complete in the backend
@@ -105,6 +123,10 @@ function Challenge() {
       <Navbar />
   
       <div className={styles.challengeBox} key={challengeName}>
+        {showPopup && <div className={styles.rotateWarning} >
+          <h1>Please rotate your mobile device</h1>
+          <p>For the best experience, rotate your phone to landscape mode.</p>
+        </div>}
         <div className={styles.navbarWrapper}>
           <ChallengeNavbar
             selectedChallenge={challengeName}
